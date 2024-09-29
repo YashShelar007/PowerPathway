92eecf99384696f4e06c1edba4fc8238730b1aac
Yash Shelar <87018353+YashShelar007@users.noreply.github.com>
2024-09-29 01:37:00 -0700

    Added Detail (Rapidapi for YT)

--- power-pathway/src/utils/fetchData.js
+++ power-pathway/src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
+    "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
